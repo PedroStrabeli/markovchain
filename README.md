c12087f8fa643916cd11c451e438181433ab0dc5
Pedro Strabeli <pedrostrabeli@gmail.com>
2017-04-11 10:52:22 -0300

    changes

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 python script for calculating availability and reliability.
 
 Forked from GFlorio. Thanks, mate.
+
+## run in python 3 with numpy, or the value will go haywire
--- sim_markov.py
+++ sim_markov.py
@@ -4,10 +4,11 @@
 import numpy as np
 
 # Dados do problema:
-MTTF = 5500  # horas
-MTTRc = 0.75  # horas
+MTTF = 6500  # horas (entre 4000 e 7500)
+MTTRc = 1.75  # horas
+MTTRp = 4.00  # horas
 
-C1 = 0.85
+C = 0.85
 C2 = 0.95
 
 dT = 0.1  # deltaT = 6 minutos
@@ -18,21 +19,23 @@ mic = 1/MTTRc
 # Cada elemento A(i, j) é a probabilidade da cadeia ir do estado i para o j.
 # Isso sai do seu desenho do modelo de Markov.
 ###### ATENCAO: ESTA MATRIZ É A TRANSPOSTA DA QUE O PROF FAZ NA AULA!!!! ######
-transicao_confiabilidade = np.array([
-        [1-2*l*dT, l*(C1+C2)*dT,      2*l*dT-(C1+C2)*l*dT],
-        [mic*dT,   1-(2*l*dT+mic*dT), 2*l*dT],
-        [0,        0,                 1]
+confiabilidade = np.array([
+        [1-2*l*dT,     l*C*dT,         l*dT,      (1-C)*l*dT    ],
+        [ mic*dT,   1-((l+mic)*dT),      0,         2*l*dT      ],
+        [ mic*dT,        0            ,1-mic*dT,     l*dT       ],
+        [    0,          0,              0,             1       ]
     ])
 
-transicao_disponibilidade = np.array([
-        [1-2*l*dT, l*(C1+C2)*dT,      2*l*dT-(C1+C2)*l*dT],
-        [mic*dT,   1-(2*l*dT+mic*dT), 2*l*dT],
-        [mic*dT,   0,                 1-mic*dT]
+disponibilidade = np.array([
+        [1-2*l*dT, l*C*dT,         l*dT,   (1-C)*l*dT],
+        [mic*dT,   1-((l+mic)*dT),      0,      2*l*dT],
+        [mic*dT,        0            ,1-mic*dT,     l*dT],
+        [mic*dT, 0, 0, 1-mic*dT]
     ])
 
 
-def distribuicao_estacionaria_simulacao(matriz_transicao, epsilon = 1e-8, retorna_historico=False):
-    """ Encontra a distribuicao estacionaria para uma cadeia de markov.
+def simulacao(matriz, epsilon = 1e-8, historico=False):
+     """ Encontra a distribuicao estacionaria para uma cadeia de markov.
     Utiliza o metodo de elevar a matriz de transicao a altas potencias ate convergir.
     
     Args:
@@ -45,30 +48,30 @@ def distribuicao_estacionaria_simulacao(matriz_transicao, epsilon = 1e-8, retorn
         np.array: O vetor distribuicao estacionaria.
         np.array: Se 'retorna_historico' for True, todos os vetores intermediarios encontrados na simulacao.
     """
-    pi = np.zeros(len(matriz_transicao))
+    pi = np.zeros(len(matriz))
     pi[0] = 1
 
     historico = [pi]
-    A = np.transpose(matriz_transicao)
+    A = np.transpose(matriz)
     
     historico.append(A.dot(pi))
     n=1
-    print("Simulando....", end="")
+    print "Iniciando a simulação....",
     while np.linalg.norm(historico[-1] - historico[-2]) > epsilon:
         n += 1
         prox_pi = np.linalg.matrix_power(A, n).dot(historico[0])
         historico.append(prox_pi) 
-    print("Pronto!")
-    return historico[-1] if not retorna_historico else (historico[-1], np.array(historico))
+    print("Simulação finalizada!")
+    return historico[-1] if not historico else (historico[-1], np.array(historico))
 
 
-print("Modelo de Disponibilidade:")
-pi = distribuicao_estacionaria_simulacao(transicao_disponibilidade)
-disp = (1-pi[-1])
-print("Disponibilidade assintótica: {}%".format(disp*100))
 
 print("\nModelo de Confiabilidade:")
 # A próxima linha vai demorar horrores.
-pi, hist = distribuicao_estacionaria_simulacao(transicao_confiabilidade, retorna_historico=True)
+pi, hist = simulacao(confiabilidade, historico=True)
 MTTF_sistema = sum([(1-x)*dT for x in hist[:,-1]])
 print("MTTF do sistema:", MTTF_sistema)
+print("Modelo de Disponibilidade:")
+pi = simulacao(disponibilidade)
+disp = (1-pi[-1])
+print("Disponibilidade assintótica: {}%".format(disp*100))
